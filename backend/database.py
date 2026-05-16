2c2911e18817f43567a6a08a5ff2f4aecbce02b9
sannthosh-wq <santhoshb7744@gmail.com>
2026-05-16 20:06:00 +0530

    update db url

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:Santhosh@localhost/attendance_system"
+DATABASE_URL = "postgresql://ems_4zv5_user:Tics5GBQlO3mQUsAhxKmW8qlxutq98vv@dpg-d8423qcvikkc738pl31g-a/ems_4zv5"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
